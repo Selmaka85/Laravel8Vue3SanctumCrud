880e33142d1b1afaad2ca4333ff615ee39bd5461
Selmaka85 <bitica_catalin85@yahoo.com>
2021-09-21 10:31:34 +0100

    first commit

---
+++ resources/js/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-center bg-secondary rounded text-light">About us!</h1>
+        <div class="jumbotron">
+            <p>This is about us section</p>
+        </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <h3 class="text-center bg-info text-white rounded p-2">This is home page!</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
